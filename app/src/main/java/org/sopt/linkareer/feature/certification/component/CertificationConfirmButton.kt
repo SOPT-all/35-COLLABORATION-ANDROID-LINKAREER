76b500e26a1485a1faa0e69165a41b9dacd169cf
ësungmin <arcn1345@gmail.com>
2024-11-20 21:32:25 +0900

    [FEAT/#7] 인증 화면 컴포넌트 패키지 추가

---
+++ app/src/main/java/org/sopt/linkareer/feature/certification/component/CertificationConfirmButton.kt
@@ -0,0 +1,20 @@
+package org.sopt.linkareer.feature.certification.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.linkareer.core.designsystem.LINKareerAndroidTheme
+
+@Composable
+fun CertificationConfirmButton(
+
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CertificationConfirmButtonPreview() {
+    LINKareerAndroidTheme {
+        CertificationConfirmButton()
+    }
+}
---
+++ app/src/main/java/org/sopt/linkareer/feature/certification/navigation/CertificationRoute.kt
@@ -0,0 +1,5 @@
+package org.sopt.linkareer.feature.certification.navigation
+
+// 임시 파일
+class CertificationRoute {
+}
