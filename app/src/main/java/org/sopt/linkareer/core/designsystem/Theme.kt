9f4be8405632428b4143a9b18e85201f4190d18d
hwiwon <hwhwhw74@naver.com>
2024-11-20 14:01:04 +0900

    [INIT/#3] 테마 시스템 구현

--- app/src/main/java/org/sopt/linkareer/core/designsystem/Theme.kt
+++ app/src/main/java/org/sopt/linkareer/core/designsystem/Theme.kt
@@ -1,58 +1,54 @@
 package org.sopt.linkareer.core.designsystem
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-        /* Other default colors to override
-        background = Color(0xFFFFFBFE),
-        surface = Color(0xFFFFFBFE),
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.White,
-        onBackground = Color(0xFF1C1B1F),
-        onSurface = Color(0xFF1C1B1F), */
+object LINKareerTheme {
+    val colors: LINKareerColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalLINKareerColors.current
+}
+
+@Composable
+fun ProvideLINKareerColorsAndTypography(
+    colors: LINKareerColors,
+    content: @Composable () -> Unit,
+) {
+    CompositionLocalProvider(
+        LocalLINKareerColors provides colors,
+        content = content
     )
+}
 
 @Composable
 fun LINKareerAndroidTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit,
+    backgroundColor: Color = defaultLINKareerColors.white,
+    content: @Composable () -> Unit
 ) {
-    val colorScheme =
-        when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    val view = LocalView.current
+
+    ProvideLINKareerColorsAndTypography(colors = defaultLINKareerColors) {
+        if (!view.isInEditMode){
+            SideEffect {
+                (view.context as Activity).window.run {
+                    statusBarColor = backgroundColor.toArgb()
+                    WindowCompat.getInsetsController(this,view).isAppearanceLightStatusBars = true
+                }
             }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
         }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+        MaterialTheme(
+            typography = Typography,
+            content = content
+        )
+    }
 }
