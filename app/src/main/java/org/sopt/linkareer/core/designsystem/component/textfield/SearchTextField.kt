124efc8515739c5e4e135431f95504b5288778fd
Hyobeen-Park <amanda0109@naver.com>
2024-11-21 15:42:33 +0900

    [CHORE/#12] 검색 textfield input text 색상 및 폰트 수정

--- app/src/main/java/org/sopt/linkareer/core/designsystem/component/textfield/SearchTextField.kt
+++ app/src/main/java/org/sopt/linkareer/core/designsystem/component/textfield/SearchTextField.kt
@@ -6,8 +6,8 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.linkareer.R
-import org.sopt.linkareer.core.designsystem.theme.Black
 import org.sopt.linkareer.core.designsystem.theme.Gray600
+import org.sopt.linkareer.core.designsystem.theme.Gray900
 import org.sopt.linkareer.core.designsystem.theme.LINKareerAndroidTheme
 import org.sopt.linkareer.core.designsystem.theme.LINKareerTheme
 
@@ -20,13 +20,13 @@ fun SearchTextField(
 ) {
     LinkareerBasicTextField(
         value = value,
-        inputTextColor = Black,
+        inputTextColor = Gray900,
         hintText = hintText,
         hintTextColor = Gray600,
         onValueChange = onValueChange,
-        inputTextStyle = LINKareerTheme.typography.body8M13,
+        inputTextStyle = LINKareerTheme.typography.body13R11,
         hintTextStyle = LINKareerTheme.typography.body8M13,
-        cursorBrush = SolidColor(Black),
+        cursorBrush = SolidColor(Gray900),
         cornerRadius = 5.dp,
         paddingHorizontal = 5.dp,
         paddingVertical = 5.dp,
