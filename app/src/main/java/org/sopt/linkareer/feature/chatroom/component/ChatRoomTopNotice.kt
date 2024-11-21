8891d435ad17f27b01d0e1241d1797738a2d9645
ësungmin <arcn1345@gmail.com>
2024-11-21 15:48:54 +0900

    [FEAT/#14] 채팅방 패키지 추가

---
+++ app/src/main/java/org/sopt/linkareer/feature/chatroom/component/ChatRoomTopNotice.kt
@@ -0,0 +1,17 @@
+package org.sopt.linkareer.feature.chatroom.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChatRoomTopNotice(
+
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable()
+fun ChatRoomTopNoticePreview() {
+
+}
---
+++ app/src/main/java/org/sopt/linkareer/feature/chatroom/navigation/ChatRoomRoute.kt
@@ -0,0 +1,4 @@
+package org.sopt.linkareer.feature.chatroom.navigation
+
+class ChatRoomRoute {
+}
