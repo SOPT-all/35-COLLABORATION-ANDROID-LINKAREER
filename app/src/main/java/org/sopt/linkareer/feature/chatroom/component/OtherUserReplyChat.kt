1bae8b53c1a274cab778fc420fcd6087836c9a9b
ësungmin <arcn1345@gmail.com>
2024-11-21 21:29:02 +0900

    [FEAT/#14] 타 유저 답장 채팅 메시지 컴포넌트 추가

---
+++ app/src/main/java/org/sopt/linkareer/feature/chatroom/component/OtherUserReplyChat.kt
@@ -0,0 +1,219 @@
+package org.sopt.linkareer.feature.chatroom.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.linkareer.R
+import org.sopt.linkareer.core.designsystem.Blue
+import org.sopt.linkareer.core.designsystem.Blue100
+import org.sopt.linkareer.core.designsystem.Blue50
+import org.sopt.linkareer.core.designsystem.Gray200
+import org.sopt.linkareer.core.designsystem.Gray400
+import org.sopt.linkareer.core.designsystem.Gray500
+import org.sopt.linkareer.core.designsystem.Gray600
+import org.sopt.linkareer.core.designsystem.Gray900
+import org.sopt.linkareer.core.designsystem.LINKareerAndroidTheme
+import org.sopt.linkareer.core.designsystem.defaultLINKareerTypography
+
+@Composable
+fun OtherUserReplyChat(
+    nickName: String,
+    isChecked: Boolean,
+    jobCategory: String,
+    imageUrl: String,
+    sender: String,
+    receivedMessage: String,
+    replyMessage: String,
+    timestamp: String,
+) {
+    Column(
+    ) {
+        OtherUserChatProfile(
+            nickName = nickName,
+            isChecked = isChecked,
+            jobCategory = jobCategory,
+            imageUrl = imageUrl,
+        )
+        OtherUserReplyBubble(
+            sender = sender,
+            receivedMessage = receivedMessage,
+            replyMessage = replyMessage,
+            timestamp = timestamp,
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_chatting_like_inactive_25),
+            contentDescription = stringResource(R.string.chatroom_reply_contentDescription),
+            modifier = Modifier
+                .padding(start = 30.dp, top = 4.dp),
+        )
+    }
+}
+
+@Composable
+fun OtherUserChatProfile(
+    nickName: String,
+    isChecked: Boolean,
+    jobCategory: String,
+    imageUrl: String,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            //model = imageUrl, 추후 , Async 이미지로 변경
+            painter = painterResource(R.drawable.img_newbie_personalitypass),
+            contentDescription = stringResource(R.string.chatroom_profile_image_contentDescription),
+            modifier = Modifier
+                .size(27.dp)
+                .clip(CircleShape),
+            //error = painterResource(R.drawable.ic_checklist_person_24)
+        )
+        Text(
+            text = nickName,
+            style = defaultLINKareerTypography.body5B11,
+            color = Gray900,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .padding(start = 3.dp, end = 8.dp)
+        )
+        if (isChecked) {
+            Image(
+                painter = painterResource(R.drawable.ic_checkbadge_home_inperson_16),
+                contentDescription = stringResource(R.string.chatroom_check_contentDescription),
+                modifier = Modifier.padding(end = 2.dp)
+            )
+        }
+        ChatJobChip(
+            job = jobCategory,
+            textColor = if (isChecked) Blue else Gray200,
+            backGroundColor = if (isChecked) Blue100 else Gray500
+        )
+    }
+}
+
+@Composable
+fun OtherUserReplyBubble(
+    sender: String,
+    receivedMessage: String,
+    replyMessage: String,
+    timestamp: String,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 30.dp),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .background(Blue50)
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.Bottom)
+        ) {
+            Text(
+                text = sender + " " + stringResource(R.string.chatroom_apply_to_sender),
+                style = defaultLINKareerTypography.body5B11,
+                color = Gray900
+            )
+            Text(
+                text = receivedMessage,
+                style = defaultLINKareerTypography.label3M11,
+                color = Gray600
+            )
+            val textWidth = with(LocalDensity.current) {
+                receivedMessage.length * 7.dp.toPx()
+            }
+
+            Box(
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { textWidth.toDp() })
+            ) {
+                HorizontalDivider(
+                    color = Gray400,
+                    thickness = 0.5.dp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            Text(
+                text = replyMessage,
+                style = defaultLINKareerTypography.body8M13,
+                color = Gray900
+            )
+        }
+        Text(
+            text = timestamp,
+            style = defaultLINKareerTypography.body13R11,
+            color = Gray600,
+            modifier = Modifier
+                .padding(start = 4.dp)
+        )
+    }
+}
+
+@Composable
+fun ChatJobChip(
+    job: String,
+    textColor: Color,
+    backGroundColor: Color,
+) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(backGroundColor)
+            .padding(5.dp),
+    ) {
+        Text(
+            text = job,
+            style = defaultLINKareerTypography.label5M8,
+            color = textColor,
+            modifier = Modifier
+                .wrapContentWidth()
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
+@Composable()
+fun OtherUserChatProfilePreview() {
+    LINKareerAndroidTheme {
+        OtherUserReplyChat(
+            nickName = "무관심한 맥",
+            isChecked = true,
+            jobCategory = "현대 자동차",
+            imageUrl = "",
+            sender = "아닌 삼백초",
+            receivedMessage = "ppt 만들 때 예쁘게꾸며야 좋나요?",
+            replyMessage = "굳이 꾸밀 필요없습니다.",
+            timestamp = "18:33",
+        )
+    }
+}
