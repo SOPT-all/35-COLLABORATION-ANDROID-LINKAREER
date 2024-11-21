c6fb25d5e36dc6421157f03e6dbe4754b3433995
Hyobeen-Park <amanda0109@naver.com>
2024-11-21 21:34:03 +0900

    [FEAT/#16] 새로고침 버튼 구현

---
+++ app/src/main/java/org/sopt/linkareer/core/designsystem/component/button/RefreshButton.kt
@@ -0,0 +1,68 @@
+package org.sopt.linkareer.core.designsystem.component.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.sopt.linkareer.R
+import org.sopt.linkareer.core.designsystem.Gray300
+import org.sopt.linkareer.core.designsystem.Gray700
+import org.sopt.linkareer.core.designsystem.LINKareerAndroidTheme
+import org.sopt.linkareer.core.designsystem.LINKareerTheme
+import org.sopt.linkareer.core.extension.noRippleClickable
+
+@Composable
+fun RefreshButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier =
+            Modifier
+                .then(modifier)
+                .border(
+                    border = BorderStroke(width = Dp.Hairline, color = Gray300),
+                    shape = RoundedCornerShape(50.dp),
+                )
+                .padding(
+                    top = 9.dp,
+                    bottom = 8.dp,
+                    start = 11.dp,
+                    end = 12.dp,
+                )
+                .noRippleClickable { onClick() },
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_arrow_refresh_12),
+            contentDescription = null,
+            tint = Gray700,
+        )
+        Text(
+            text = stringResource(R.string.chatting_home_refresh),
+            style = LINKareerTheme.typography.label4M10,
+            color = Gray700,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RefreshButtonPreview() {
+    LINKareerAndroidTheme {
+        RefreshButton(
+            onClick = {},
+        )
+    }
+}
