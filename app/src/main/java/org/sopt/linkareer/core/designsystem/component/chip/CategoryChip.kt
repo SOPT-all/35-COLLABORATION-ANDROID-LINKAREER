920c9a5e7c1c6c2028aec0b1f7d74c93ab407bfc
Hyobeen-Park <amanda0109@naver.com>
2024-11-21 20:47:23 +0900

    [FEAT/#16] Category 칩 구현

---
+++ app/src/main/java/org/sopt/linkareer/core/designsystem/component/chip/CategoryChip.kt
@@ -0,0 +1,38 @@
+package org.sopt.linkareer.core.designsystem.component.chip
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.linkareer.core.designsystem.Gray300
+import org.sopt.linkareer.core.designsystem.Gray800
+import org.sopt.linkareer.core.designsystem.LINKareerAndroidTheme
+
+@Composable
+fun CategoryChip(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    ColourFilledBasicChip(
+        text = text,
+        textColor = Gray800,
+        backgroundColor = Gray300,
+        paddingValues = PaddingValues(vertical = 2.dp, horizontal = 6.dp),
+        cornerRadius = 4.dp,
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryChipPreview() {
+    LINKareerAndroidTheme {
+        CategoryChip(
+            text = "카테고리 칩",
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
+}
