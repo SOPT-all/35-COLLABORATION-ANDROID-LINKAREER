0d1f9bae247c19aeb0f93b5c483e2eef8dd18204
Hyobeen-Park <amanda0109@naver.com>
2024-11-22 12:32:04 +0900

    [MOD/#16] Image painterResource -> ImageVector로 수정

--- app/src/main/java/org/sopt/linkareer/core/designsystem/component/button/RefreshButton.kt
+++ app/src/main/java/org/sopt/linkareer/core/designsystem/component/button/RefreshButton.kt
@@ -10,8 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -45,7 +46,7 @@ fun RefreshButton(
                 .noRippleClickable { onClick() },
     ) {
         Icon(
-            painter = painterResource(R.drawable.ic_arrow_refresh_12),
+            imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_refresh_12),
             contentDescription = null,
             tint = Gray700,
         )
