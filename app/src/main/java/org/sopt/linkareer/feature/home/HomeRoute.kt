0b5f8fa3b230a0d1b6522ca9aa9e52a6012ce8b7
Hyobeen-Park <amanda0109@naver.com>
2024-11-26 13:29:33 +0900

    [FEAT/#31] 홈 탑바 부분 구현

--- app/src/main/java/org/sopt/linkareer/feature/home/HomeRoute.kt
+++ app/src/main/java/org/sopt/linkareer/feature/home/HomeRoute.kt
@@ -1,11 +1,26 @@
 package org.sopt.linkareer.feature.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.linkareer.R
+import org.sopt.linkareer.core.designsystem.component.tapbar.HomeTapBar
+import org.sopt.linkareer.core.designsystem.component.textfield.SearchTextField
+import org.sopt.linkareer.core.designsystem.theme.Blue
+import org.sopt.linkareer.core.designsystem.theme.Gray300
+import org.sopt.linkareer.core.designsystem.theme.LINKareerAndroidTheme
+import org.sopt.linkareer.feature.home.component.HomeTitle
 
 @Composable
 fun HomeRoute(
@@ -19,8 +34,57 @@ fun HomeScreen(
     paddingValues: PaddingValues,
 ) {
     Column(
-        modifier = Modifier.padding(paddingValues),
+        modifier =
+            Modifier
+                .padding(paddingValues),
     ) {
-        Text("HOME")
+        var searchText by remember { mutableStateOf("") }
+
+        SearchTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            hintText = stringResource(R.string.home_search_textfield_hint),
+            modifier =
+                Modifier
+                    .background(Blue)
+                    .padding(horizontal = 17.dp, vertical = 12.dp),
+        )
+
+        HomeTapBar(
+            onTabClick = {},
+        )
+
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Gray300,
+        )
+
+        HomeTitle(
+            blueText = stringResource(R.string.home_title_name),
+            blackText = stringResource(R.string.home_title_interest_announcement),
+            grayText = stringResource(R.string.home_sub_title_interest_announcement),
+            chipList =
+                listOf(
+                    stringResource(R.string.home_chip_interest_announcement_1),
+                    stringResource(R.string.home_chip_interest_announcement_2),
+                    stringResource(R.string.home_chip_interest_announcement_3),
+                ),
+            modifier =
+                Modifier
+                    .padding(
+                        start = 15.dp,
+                        end = 17.dp,
+                        top = 20.dp,
+                        bottom = 8.dp,
+                    ),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    LINKareerAndroidTheme {
+        HomeScreen(PaddingValues(1.dp))
     }
 }
---
+++ app/src/main/java/org/sopt/linkareer/feature/home/component/HomeTitle.kt
@@ -0,0 +1,99 @@
+package org.sopt.linkareer.feature.home.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.sopt.linkareer.R
+import org.sopt.linkareer.core.designsystem.component.chip.BlueOutlinedChip
+import org.sopt.linkareer.core.designsystem.theme.Black
+import org.sopt.linkareer.core.designsystem.theme.Blue
+import org.sopt.linkareer.core.designsystem.theme.Gray600
+import org.sopt.linkareer.core.designsystem.theme.Gray800
+import org.sopt.linkareer.core.designsystem.theme.LINKareerTheme
+import org.sopt.linkareer.core.extension.noRippleClickable
+
+@Composable
+fun HomeTitle(
+    blueText: String,
+    blackText: String,
+    grayText: String,
+    chipList: List<String>,
+    onButtonClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier =
+            modifier
+                .fillMaxWidth(),
+    ) {
+        Column(
+            modifier =
+                Modifier
+                    .weight(1f),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = blueText,
+                    style = LINKareerTheme.typography.title2B17,
+                    color = Blue,
+                )
+                Text(
+                    text = blackText,
+                    style = LINKareerTheme.typography.title1B18,
+                    color = Black,
+                    modifier =
+                        Modifier
+                            .padding(start = 4.dp),
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                for (text in chipList) {
+                    BlueOutlinedChip(
+                        text = text,
+                        modifier =
+                            Modifier
+                                .padding(end = 4.dp),
+                    )
+                }
+
+                Text(
+                    text = grayText,
+                    style = LINKareerTheme.typography.body14R10,
+                    color = Gray600,
+                )
+            }
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier =
+                Modifier
+                    .padding(vertical = 4.dp)
+                    .noRippleClickable { onButtonClick() }
+                    .padding(vertical = 10.dp),
+        ) {
+            Text(
+                text = stringResource(R.string.home_more_button),
+                style = LINKareerTheme.typography.body10M11,
+                color = Gray800,
+            )
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_right_black_12),
+                contentDescription = null,
+            )
+        }
+    }
+}
