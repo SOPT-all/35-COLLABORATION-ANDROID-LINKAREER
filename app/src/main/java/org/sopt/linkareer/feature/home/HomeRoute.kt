e4b2725159333cd100ed1da88ce50fc8ba04d910
Hyobeen-Park <amanda0109@naver.com>
2024-11-26 14:29:58 +0900

    [FEAT/#31] 홈 추천 공고 및 footer 구현

--- app/src/main/java/org/sopt/linkareer/feature/home/HomeRoute.kt
+++ app/src/main/java/org/sopt/linkareer/feature/home/HomeRoute.kt
@@ -1,9 +1,11 @@
 package org.sopt.linkareer.feature.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,6 +22,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.linkareer.R
+import org.sopt.linkareer.core.designsystem.component.chip.FilteringChip
+import org.sopt.linkareer.core.designsystem.component.footer.FooterText
+import org.sopt.linkareer.core.designsystem.component.footer.MainFooter
 import org.sopt.linkareer.core.designsystem.component.tapbar.HomeTapBar
 import org.sopt.linkareer.core.designsystem.component.textfield.SearchTextField
 import org.sopt.linkareer.core.designsystem.theme.Blue
@@ -129,6 +134,62 @@ fun HomeScreen(
             Box(
                 modifier = Modifier.size(width = 326.dp, height = 132.dp)
             )
+        }
+        item {
+            Column(
+                modifier = Modifier
+                    .background(Blue50)
+            ) {
+                HomeTitle(
+                    blueText = stringResource(R.string.home_title_name),
+                    blackText = stringResource(R.string.home_title_recommend_announcement),
+                    grayText = stringResource(R.string.home_sub_title_recommend_announcement),
+                    chipList =
+                    listOf(
+                        stringResource(R.string.home_chip_recommend_announcement_1),
+                        stringResource(R.string.home_chip_recommend_announcement_2),
+                        stringResource(R.string.home_chip_recommend_announcement_3),
+                    ),
+                    modifier =
+                    Modifier
+                        .padding(
+                            start = 15.dp,
+                            end = 17.dp,
+                            top = 20.dp,
+                            bottom = 8.dp,
+                        ),
+                )
+                Row(
+                    modifier = Modifier.padding(start = 15.dp)
+                ) {
+                    FilteringChip(
+                        text = stringResource(R.string.home_filtering_employment_news),
+                        isSelected = true,
+                    )
+                    FilteringChip(
+                        text = stringResource(R.string.home_filtering_activities),
+                        isSelected = false,
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                    )
+                }
+
+                LazyRow(
+                    contentPadding = PaddingValues(horizontal = 15.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .padding(top = 12.dp, bottom = 40.dp)
+                ) {
+                    items(6) {
+                        Box(
+                            modifier = Modifier
+                                .size(width = 120.dp, height = 216.dp)
+                                .background(Blue)
+                        )
+                    }
+                }
+            }
+            MainFooter()
         }
     }
 }
