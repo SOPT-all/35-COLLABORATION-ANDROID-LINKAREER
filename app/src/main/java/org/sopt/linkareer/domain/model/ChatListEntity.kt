46c42b31e0f4fec4bebaf760ae87c6f642df0456
ësungmin <arcn1345@gmail.com>
2024-11-27 21:52:59 +0900

    [FEAT/#40] Dto, Entity 추가

---
+++ app/src/main/java/org/sopt/linkareer/domain/model/ChatListEntity.kt
@@ -0,0 +1,38 @@
+package org.sopt.linkareer.domain.model
+
+data class ChatListEntity(
+    val chatRoomName: String,
+    val chatParticipantsCount: Int,
+    val chatPartner: ChatPartnerEntity,
+    val myChat: MyChatEntity
+)
+
+data class ChatPartnerEntity(
+    val partnerName: String,
+    val isBlueChecked: Boolean,
+    val tag: TagEntity,
+    val chatList: List<ChatEntity>
+)
+
+data class TagEntity(
+    val companyName: String,
+    val job: String
+)
+
+data class ChatEntity(
+    val message: String,
+    val isReplied: Boolean,
+    val likes: Int,
+    val pressedLike: Boolean,
+    val createdTime: String,
+    val reply: ReplyEntity?
+)
+
+data class ReplyEntity(
+    val replyMessage: String?,
+    val repliedMessageSenderName: String?
+)
+
+data class MyChatEntity(
+    val chatList: List<ChatEntity>
+)
