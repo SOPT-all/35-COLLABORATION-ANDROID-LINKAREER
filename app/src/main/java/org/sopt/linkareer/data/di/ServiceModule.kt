44b73cf5205e9415ace230bf9feb9059fa2683a1
ësungmin <arcn1345@gmail.com>
2024-11-27 21:54:19 +0900

    [FEAT/#40] Module 추가

--- app/src/main/java/org/sopt/linkareer/data/di/ServiceModule.kt
+++ app/src/main/java/org/sopt/linkareer/data/di/ServiceModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.sopt.linkareer.data.service.ChatService
 import org.sopt.linkareer.data.service.DummyService
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -15,4 +16,9 @@ object ServiceModule {
     @Singleton
     fun provideDummyService(retrofit: Retrofit): DummyService =
         retrofit.create(DummyService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideChatService(retrofit: Retrofit): ChatService =
+        retrofit.create(ChatService::class.java)
 }
