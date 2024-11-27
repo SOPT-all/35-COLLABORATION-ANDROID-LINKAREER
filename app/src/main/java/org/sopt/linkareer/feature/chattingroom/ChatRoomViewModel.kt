942f849b729d52ea3ec5616a11975dcba0fce1aa
ësungmin <arcn1345@gmail.com>
2024-11-28 01:58:06 +0900

    [FEAT/#40] ChatRoomViewModel API 호출 함수 추가 및 MVI 적용

--- app/src/main/java/org/sopt/linkareer/feature/chattingroom/ChatRoomViewModel.kt
+++ app/src/main/java/org/sopt/linkareer/feature/chattingroom/ChatRoomViewModel.kt
@@ -1,15 +1,40 @@
 package org.sopt.linkareer.feature.chattingroom
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import org.sopt.linkareer.core.state.UiState
 import org.sopt.linkareer.data.model.usecase.GetChatListUseCase
-import org.sopt.linkareer.domain.model.ChatListEntity
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatRoomViewModel @Inject constructor(
     private val getChatListUseCase: GetChatListUseCase
 ) : ViewModel() {
+    var chatRoomState: MutableStateFlow<UiState<ChatRoomState>> = MutableStateFlow<UiState<ChatRoomState>>(UiState.Empty)
+        private set
+
+    fun getChatList() {
+        chatRoomState.value = UiState.Loading
+        viewModelScope.launch {
+            getChatListUseCase().fold(
+                onSuccess = { chatListEntity ->
+                    chatRoomState.value = UiState.Success(
+                        ChatRoomState(chatListEntity = chatListEntity)
+                    )
+                    Timber.tag("ChatRoomViewModel").d("성공 : $chatListEntity")
+                },
+                onFailure = {
+                    chatRoomState.value = UiState.Failure(
+                        msg = it.message.toString()
+                    )
+                    Timber.tag("ChatRoomViewModel").d("실패 : ${it.message}")
+                }
+            )
+        }
+    }
 
 }
