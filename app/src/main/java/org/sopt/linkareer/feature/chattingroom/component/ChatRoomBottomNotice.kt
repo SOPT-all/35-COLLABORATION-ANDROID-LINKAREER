c4265c518afed19e1dc204672e00dcc25fceaadb
ësungmin <arcn1345@gmail.com>
2024-11-28 01:03:51 +0900

    [FEAT/#40] ChatRoomBottomNotice x 버튼 누르면 사라짐

--- app/src/main/java/org/sopt/linkareer/feature/chattingroom/component/ChatRoomBottomNotice.kt
+++ app/src/main/java/org/sopt/linkareer/feature/chattingroom/component/ChatRoomBottomNotice.kt
@@ -3,7 +3,7 @@ package org.sopt.linkareer.feature.chattingroom.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,15 +29,18 @@ import org.sopt.linkareer.core.designsystem.theme.Gray200
 import org.sopt.linkareer.core.designsystem.theme.Gray700
 import org.sopt.linkareer.core.designsystem.theme.LINKareerAndroidTheme
 import org.sopt.linkareer.core.designsystem.theme.LINKareerTheme
+import org.sopt.linkareer.core.extension.noRippleClickable
 
 @Composable
 fun ChatRoomBottomNotice(
     onClickToCheck: () -> Unit,
-    onClickDelete: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier =
+    var isVisible by remember { mutableStateOf(true) }
+
+    if(isVisible) {
+        Box(
+            modifier =
             modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(10.dp))
@@ -46,41 +51,43 @@ fun ChatRoomBottomNotice(
                     shape = RoundedCornerShape(10.dp),
                 )
                 .padding(8.dp),
-    ) {
-        Row(
-            modifier =
+        ) {
+            Row(
+                modifier =
                 Modifier
                     .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_certification_check_bottom),
-                contentDescription = stringResource(R.string.chatroom_notice_contentDescription),
-            )
-            Spacer(modifier = Modifier.padding(start = 4.dp))
-            Text(
-                modifier =
-                    Modifier
-                        .clickable { onClickToCheck() },
-                text = stringResource(R.string.chatroom_notice_link),
-                style =
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_certification_check_bottom),
+                    contentDescription = stringResource(R.string.chatroom_notice_contentDescription),
+                )
+                Spacer(modifier = Modifier.padding(start = 4.dp))
+                Text(
+                    text = stringResource(R.string.chatroom_notice_link),
+                    style =
                     LINKareerTheme.typography.body11M10.copy(
                         textDecoration = TextDecoration.Underline,
                     ),
-                color = Blue,
-            )
-            Text(
-                text = stringResource(R.string.chatroom_notice_link_description),
-                style = LINKareerTheme.typography.body11M10,
-                color = Gray700,
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Image(
-                painter = painterResource(R.drawable.ic_chatroom_close),
-                contentDescription = null,
-                Modifier
-                    .clickable { onClickDelete() },
-            )
+                    color = Blue,
+                    modifier =
+                    Modifier
+                        .noRippleClickable { onClickToCheck() },
+                )
+                Text(
+                    text = stringResource(R.string.chatroom_notice_link_description),
+                    style = LINKareerTheme.typography.body11M10,
+                    color = Gray700,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+
+                Image(
+                    painter = painterResource(R.drawable.ic_chatroom_close),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .noRippleClickable { isVisible = false }, // 이 뷰 닫기
+                )
+            }
         }
     }
 }
@@ -94,7 +101,6 @@ fun ChatRoomBottomNoticePreview() {
     LINKareerAndroidTheme {
         ChatRoomBottomNotice(
             onClickToCheck = {},
-            onClickDelete = {},
         )
     }
 }
