f2e11d9f4f1d15fed482ac7059420428c79e7911
Hyobeen-Park <amanda0109@naver.com>
2024-11-28 02:09:53 +0900

    [FEAT/#38] 채팅홈 아이템 연결

--- app/src/main/java/org/sopt/linkareer/domain/model/ChattingRoomEntity.kt
+++ app/src/main/java/org/sopt/linkareer/domain/model/ChattingRoomEntity.kt
@@ -3,6 +3,7 @@ package org.sopt.linkareer.domain.model
 import org.sopt.linkareer.R
 
 data class ChattingRoomEntity(
+    val id: Int,
     val companyImage: Int,
     val company: String,
     val title: String,
@@ -14,6 +15,7 @@ data class ChattingRoomEntity(
 fun getChattingRoomList(): List<ChattingRoomEntity> =
     listOf(
         ChattingRoomEntity(
+            id = 1,
             companyImage = R.drawable.img_chattingroom_inperson_lgcns_70,
             company = "LG CNS",
             title = "[LG CNS] 2025년 동계 체험형 인턴 채용",
@@ -22,6 +24,7 @@ fun getChattingRoomList(): List<ChattingRoomEntity> =
             isInPerson = true,
         ),
         ChattingRoomEntity(
+            id = 2,
             companyImage = R.drawable.img_chattingroom_inperson_samsung_70,
             company = "삼성전자",
             title = "[삼성전자] 2025년 상반기 체험형 인턴 채용",
@@ -30,6 +33,7 @@ fun getChattingRoomList(): List<ChattingRoomEntity> =
             isInPerson = true,
         ),
         ChattingRoomEntity(
+            id = 3,
             companyImage = R.drawable.img_chattingroom_inperson_amore_70,
             company = "AMORE PACIFIC",
             title = "[AMORE PACIFIC] 2025년 동계 체험형 인턴 채용",
@@ -38,6 +42,7 @@ fun getChattingRoomList(): List<ChattingRoomEntity> =
             isInPerson = true,
         ),
         ChattingRoomEntity(
+            id = 4,
             companyImage = R.drawable.img_chattingroom_inperson_lgcns_70,
             company = "LG CNS",
             title = "[LG CNS] 2025년 동계 체험형 인턴 채용",
@@ -46,6 +51,7 @@ fun getChattingRoomList(): List<ChattingRoomEntity> =
             isInPerson = true,
         ),
         ChattingRoomEntity(
+            id = 5,
             companyImage = R.drawable.img_chattingroom_inperson_samsung_70,
             company = "삼성전자",
             title = "[삼성전자] 2025년 상반기 체험형 인턴 채용",
@@ -54,6 +60,7 @@ fun getChattingRoomList(): List<ChattingRoomEntity> =
             isInPerson = true,
         ),
         ChattingRoomEntity(
+            id = 6,
             companyImage = R.drawable.img_chattingroom_inperson_amore_70,
             company = "AMORE PACIFIC",
             title = "[AMORE PACIFIC] 2025년 동계 체험형 인턴 채용",
--- app/src/main/java/org/sopt/linkareer/feature/chattinghome/component/ChattingHomeRoomListItem.kt
+++ app/src/main/java/org/sopt/linkareer/feature/chattinghome/component/ChattingHomeRoomListItem.kt
@@ -1,19 +1,13 @@
 package org.sopt.linkareer.feature.chattinghome.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import org.sopt.linkareer.core.designsystem.theme.LINKareerTheme
-import org.sopt.linkareer.core.extension.noRippleClickable
+import org.sopt.linkareer.core.designsystem.component.chattingroom.ChattingRoomInPerson
 import org.sopt.linkareer.domain.model.ChattingRoomEntity
 
 @Composable
@@ -28,16 +22,21 @@ fun ChattingHomeRoomListItem(
                 .fillMaxWidth()
                 .padding(start = 16.dp, end = 15.dp),
     ) {
-        items(10) {
-            Box(
-                modifier =
-                    Modifier
-                        .height(94.dp)
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(LINKareerTheme.colors.gray500)
-                        .noRippleClickable { onClick() },
-            )
+        items(
+            count = roomList.size,
+            key = { roomList[it].id },
+        ) { room ->
+            with(roomList[room]) {
+                ChattingRoomInPerson(
+                    image = companyImage,
+                    categories = categories,
+                    company = company,
+                    chattingTitle = title,
+                    participationPerson = participation,
+                    onClick = onClick,
+                    isInPersonConversation = isInPerson,
+                )
+            }
         }
     }
 }
