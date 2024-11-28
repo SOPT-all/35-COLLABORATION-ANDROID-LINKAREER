35d0eb9f3ab7334130ce4e3aa2ae242f0a6c2b86
ësungmin <arcn1345@gmail.com>
2024-11-28 17:04:32 +0900

    [FEAT/#45] MainScreen NavHost에 인증 안내 화면 Graph 추가

--- app/src/main/java/org/sopt/linkareer/feature/main/MainScreen.kt
+++ app/src/main/java/org/sopt/linkareer/feature/main/MainScreen.kt
@@ -23,6 +23,7 @@ import org.sopt.linkareer.core.designsystem.theme.Gray900
 import org.sopt.linkareer.core.designsystem.theme.LINKareerTheme
 import org.sopt.linkareer.core.designsystem.theme.White
 import org.sopt.linkareer.core.util.NoRippleInteraction
+import org.sopt.linkareer.feature.certification.navigation.certificationGuideGuide
 import org.sopt.linkareer.feature.chattinghome.navigation.chattingHomeGraph
 import org.sopt.linkareer.feature.chattingroom.navigation.chattingRoomGraph
 import org.sopt.linkareer.feature.home.navigation.homeNavGraph
@@ -78,6 +79,9 @@ fun MainScreen(
                     navHostController = navigator.navController,
                 )
                 chattingRoomGraph(
+                    navHostController = navigator.navController,
+                )
+                certificationGuideGuide(
                     navHostController = navigator.navController,
                 )
             }
