73813771be403161835abcf9d893463d83b20ad0
ësungmin <arcn1345@gmail.com>
2024-11-29 02:36:51 +0900

    [FEAT/#49] ChatService 채팅 목록 조회 API 엔드포인트 수정

--- app/src/main/java/org/sopt/linkareer/data/service/ChatService.kt
+++ app/src/main/java/org/sopt/linkareer/data/service/ChatService.kt
@@ -4,6 +4,6 @@ import org.sopt.linkareer.data.model.response.ChatListResponse
 import retrofit2.http.GET
 
 interface ChatService {
-    @GET("/api/v1/chatting/history")
+    @GET("api/v1/chatting/history")
     suspend fun getChatList(): ChatListResponse
 }
